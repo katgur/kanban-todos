ece1a4322944675b6416343d5fe13cf3b7d54c46
katgur <mitsutanaka29@gmail.com>
2023-12-05 19:00:30 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import MainPage from './pages/MainPage'
+import TaskPage from './pages/TaskPage'
+import FullPage from './pages/FullPage'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Routes>
+        <Route path='/' element={<MainPage />}>
+          <Route path='create' element={<TaskPage />} />
+          <Route path='edit/:id' element={<TaskPage />} />
+        </Route>
+        <Route path='full/:id' element={<FullPage />} />
+      </Routes>
+    </DndProvider>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import { BrowserRouter as Router } from 'react-router-dom'
+import store from './app/store.js'
+import { Provider } from 'react-redux'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+)
