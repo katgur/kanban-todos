b521e2bff9cd07589ccd3620ed788f9cc12c0293
katgur <mitsutanaka29@gmail.com>
2023-12-07 13:07:54 +0300

    fixed drop area

--- src/components/DropArea.tsx
+++ src/components/DropArea.tsx
@@ -1,6 +1,6 @@
 import { useDrop } from 'react-dnd'
-import { useDispatch } from 'react-redux'
-import { update } from '../features/todoSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { getTodoById, update } from '../features/todoSlice'
 import { updateTodo } from '../data/todoApi'
 import React from 'react'
 import { Status, Todo } from '../types'
@@ -10,14 +10,18 @@ interface DropAreaProps {
     status: Status,
 }
 
+interface DropAreaItem {
+    todo: Todo,
+}
+
 function DropArea({ children, status }: DropAreaProps) {
     const dispatch = useDispatch();
 
     const [_, drop] = useDrop(() => {
         return {
             accept: 'task',
-            drop: (todo: Todo) => {
-                const newTask = { ...todo, status };
+            drop: (item: DropAreaItem) => {
+                const newTask = { ...item.todo, status };
                 updateTodo(newTask)
                     .then((data: Todo) => {
                         dispatch(update(data));
